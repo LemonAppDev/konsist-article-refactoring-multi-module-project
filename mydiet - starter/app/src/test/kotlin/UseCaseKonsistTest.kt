8e76cbe019747ff86c2ca4ce3ce7a242c9dda7e7
Igor Wojda <igor.wojda@gmail.com>
2023-08-26 21:11:42 +0200

    Add empty UseCaseKonsistTest class

--- mydiet - starter/app/src/test/kotlin/UseCaseKonsistTest.kt
+++ mydiet - starter/app/src/test/kotlin/UseCaseKonsistTest.kt
@@ -3,15 +3,5 @@ import com.lemonappdev.konsist.api.verify.assert
 import org.junit.jupiter.api.Test
 
 class UseCaseKonsistTest {
-    @Test
-    fun `featureCaloryCalculator classes with 'UseCase' suffix should have a public method named 'invoke'`() {
-        Konsist
-            .scopeFromModule("featureCaloryCalculator")
-            .classes()
-            .assert {
-                it.containsFunction { function ->
-                    function.name == "invoke" && function.hasPublicOrDefaultModifier
-                }
-            }
-    }
+    // Add Konsist Test Here
 }
